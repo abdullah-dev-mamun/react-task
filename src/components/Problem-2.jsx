dae5172b53f528c251a5c23c1bc0b6ea67057fb3
@abdullah-sumon <aalsumon@gmail.com>
2023-09-25 01:53:41 +0600

    Problem 2 solve

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -5,12 +5,17 @@ const Problem2 = () => {
   const [data, setData] = useState({});
   const [values, setValues] = useState({
     query: "",
+    type: "all",
   });
 
-  useEffect(() => {}, [values.query]);
+  useEffect(() => {
+    setTimeout(() => {
+      handleSearch(values.type);
+    }, 500);
+  }, [values.query]);
 
   useEffect(() => {
-    console.log(data);
+    // console.log(data);
   }, [data]);
 
   function handleChange(e) {
@@ -25,21 +30,31 @@ const Problem2 = () => {
   }
 
   async function getDataFromApi(type) {
-    let apiUrl = "https://contact.mediusware.com/api/contacts/";
+    let apiUrl = `https://contact.mediusware.com/api/contacts/${
+      values.query ? `?search=${values.query}` : ``
+    }`;
     if (type == "us") {
-      apiUrl =
-        "https://contact.mediusware.com/api/country-contacts/United%20States/";
+      apiUrl = `https://contact.mediusware.com/api/country-contacts/United%20States/${
+        values.query ? `?search=${values.query}` : ``
+      }`;
     }
+    // console.log(apiUrl);
     const response = await fetch(apiUrl);
     return response.json();
   }
 
   async function handleOpen(type) {
+    handleChange({ target: { name: "type", value: type } });
     setShowModal(true);
     setData(await getDataFromApi(type));
   }
 
+  async function handleSearch(type) {
+    setData(await getDataFromApi(type));
+  }
+
   function onClose() {
+    handleChange({ target: { name: "query", value: "" } });
     setData({});
     setShowModal(false);
   }
@@ -73,51 +88,58 @@ const Problem2 = () => {
 
   const renderModal = () => {
     return (
-      <div className="w-100 h-100  bg-light position-fixed top-0 left-0 overflow-scroll">
-        <h4 className="text-center text-uppercase mb-5">Contact List</h4>
-        <form className="row gy-2 gx-3 align-items-center mb-4">
-          <div className="col-auto w-50">
-            <input
-              name="query"
-              value={values.query}
-              onChange={handleChange}
-              type="text"
-              className="form-control w-100"
-              placeholder="Search..."
-            />
+      <div
+        style={{ backgroundColor: "#5A5A5A" }}
+        className="w-100 h-100  position-fixed top-0 start-0 overflow-scroll overflow-x-hidden py-5"
+      >
+        <div style={{ width: "800px" }} className="mx-auto">
+          <h4 className="text-center text-uppercase mb-5 text-white font-monospace">
+            Contact List
+          </h4>
+          <form className="row gy-2 gx-3 align-items-center mb-4">
+            <div className="col-auto w-50">
+              <input
+                name="query"
+                value={values.query}
+                onChange={handleChange}
+                type="text"
+                className="form-control w-100"
+                placeholder="Search..."
+              />
+            </div>
+          </form>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">S/L</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Country</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.results && data.results.length
+                ? data.results.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{item.id}</td>
+                        <td>{item.phone}</td>
+                        <td>{item.country.name}</td>
+                      </tr>
+                    );
+                  })
+                : null}
+            </tbody>
+          </table>
+          <div className="d-flex justify-content-center gap-3">
+            {renderButtons()}
+            <button
+              onClick={onClose}
+              className="btn btn-lg btn-outline-danger"
+              type="button"
+            >
+              Close
+            </button>
           </div>
-        </form>
-        <table className="table table-striped ">
-          <thead>
-            <tr>
-              <th scope="col">S/L</th>
-              <th scope="col">Phone Number</th>
-              <th scope="col">Country</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.results && data.results.length
-              ? data.results.map((item, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{item.id}</td>
-                      <td>{item.phone}</td>
-                      <td>{item.country.name}</td>
-                    </tr>
-                  );
-                })
-              : null}
-          </tbody>
-        </table>
-        <div className="d-flex justify-content-center gap-3">
-          {renderButtons()}
-          <button
-            onClick={onClose}
-            className="btn btn-lg btn-outline-danger"
-            type="button"
-          >
-            Close
-          </button>
         </div>
       </div>
     );
